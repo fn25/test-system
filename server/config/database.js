a1da27d576fd5c59e949fa0e256ee570a5657fca
fn25 <xushvaqtovsardor032@gmail.com>
2025-10-23 21:03:53 +0500

    fix: Remove deprecated MongoDB connection options

--- server/config/database.js
+++ server/config/database.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://gaday099_db_user:your_password@cluster0.njpkhy2.mongodb.net/testlash_tizmi?retryWrites=true&w=majority&appName=Cluster0';
     
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI);
 
     console.log('✅ MongoDB connected successfully');
     console.log(`📊 Database: ${mongoose.connection.name}`);
